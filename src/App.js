c73b9e920afe4e95802b9db607065b55e7bc73cb
Ethan Davidson <ethanld@outlook.com>
2023-03-10 21:10:01 -0700

    imported ElectronicPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './Header.js'
+import ElectronicPage from './ElectronicPage.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ElectronicPage />
     </div>
   );
 }

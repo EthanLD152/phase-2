72da9560ec097040f3ed99e2f16b3a40f17a00c7
Ethan Davidson <ethanld@outlook.com>
2023-03-10 20:46:07 -0700

    Header added

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>TechWave Electronics</h1>
+      <h3>CART</h3>
+    </header>
+  )
+}
+
+export default Header;

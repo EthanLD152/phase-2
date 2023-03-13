d2eead4adc31e027d4d3309f94f444afc802d6c3
Ethan Davidson <ethanld@outlook.com>
2023-03-13 11:48:33 -0700

    added search bar

--- src/Search.js
+++ src/Search.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 function Search({search, onSearch}) {
 
+  return (
+    <div className="search">
+      <label>Search</label>
+      <input type="text" id="search" palceholder="Product" value={search} onChange={(e) => onSearch(e.target.value)}/>
+    </div>
+ )
 }
 
 export default Search;

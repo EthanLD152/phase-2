19133becfc670bf8a4be26c58adf05295de84714
Ethan Davidson <ethanld@outlook.com>
2023-03-13 12:33:25 -0700

    maping out products and creating the classes

--- src/Product.js
+++ src/Product.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 function Product({ products }) {
   const {title, thumbnail, price, description } = products;
-  return (
 
+  return (
+    <div className="container">
+      <div className="product-grid">
+        {products.map((item) => {
+          <div className="prodcut-card">
+        })}
+      </div>
+    </div>
   )
 }

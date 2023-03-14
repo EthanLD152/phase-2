5c05627063133b95558447662dee5aca1097c132
Ethan Davidson <ethanld@outlook.com>
2023-03-13 21:10:11 -0700

    chnaged title

--- src/ElectronicPage.js
+++ src/ElectronicPage.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Search from './Search.js'
+import Product from './Product.js'
+import './App.css'
 
 function ElectronicPage() {
   const [products, setProduct] = useState([])
@@ -18,8 +20,9 @@ function ElectronicPage() {
 
   return(
     <>
-    <h2>Product Catelog</h2>
+    <h1>This Weeks Featured products</h1>
     <Search search={search} onSearchChange={setSearch}/>
+    <Product products={showProducts} />
     </>
   );
 }
